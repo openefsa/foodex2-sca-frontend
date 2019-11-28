91b12f353b502455f6cd91d41daf1d66e95f572a
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2019-11-28 17:33:34 +0100

    removed usless properties, moved fetch data in appropriate web component, removed unused web component from render

--- src/wc-body.js
+++ src/wc-body.js
@@ -15,53 +15,19 @@ export class WcBody extends LitElement {
                 type: Map
             },
             baseterm: {
-                type: Array()
+                type: Array
             },
             facets: {
-                type: Array()
-            },
-            basetermSugg: {
-                type: String
-            },
-            facetsSugg: {
-                type: String
+                type: Array
             }
         }
     }
 
     constructor() {
         super()
-        this.ppText = new Map();
-        this.baseterm = new Array();
-        this.facets = new Array();
-    }
-
-    // call the flask interface for receiving suggestions
-    getSuggestions(){
-
-        // print error if no baseterm
-        if (this.baseterm.length <= 0 || !this.baseterm) {
-            console.log("Please classify at least the baseterm.");
-            return;
-        }
-
-        // function below will run createConnection.php 
-        $.ajax({
-            url: "http://127.0.0.1:5000/getSuggestions",
-            type: "POST",
-            data: JSON.stringify({ baseterm: this.baseterm, facets: this.facets }), // input variable to php
-            contentType: "application/json; charset=utf-8",
-            success: function (data) {
-                // print the returned code
-                console.log(data);
-                this.basetermSugg = data;
-            },
-            error: function (xhr, status, error) {
-                // executed if something went wrong during call
-                // status 0 - when load is interrupted
-                if (xhr.status > 0) alert('got error: ' + status, error);
-            }
-        });
+        this.ppText = [];
+        this.baseterm = [];
+        this.facets = [];
     }
 
     render() {
@@ -71,11 +37,9 @@ export class WcBody extends LitElement {
                 <!-- component for analysing the food description inserted -->
                 <wc-body-analyser @analysed="${(e) => (this.ppText = e.detail.ppText)}"></wc-body-analyser>
                 <!-- component for tagging the words for baseterm -->
-                <wc-body-classifier .ppText="${this.ppText}" @classified="${(e) => (this.baseterm = e.detail.baseterm, this.facets = e.detail.facets)}" @sugg="${(e) => this.getSuggestions()}"></wc-body-classifier>
-                <!-- component for selecting best suggested terms -->
-                <wc-body-advisor></wc-body-advisor>
+                <wc-body-classifier .ppText="${this.ppText}" @classified="${(e) => (this.baseterm = e.detail.baseterm, this.facets = e.detail.facets)}"></wc-body-classifier>
                 <!-- component for getting the foodex2 code -->
-                <wc-body-getcode .ppText="${this.ppText}" .baseterm="${this.baseterm}" .facets="${this.facets}"></wc-body-getcode>
+                <wc-body-getcode .baseterm="${this.baseterm}" .facets="${this.facets}"></wc-body-getcode>
             </div>
         `
     }
