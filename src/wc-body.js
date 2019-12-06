bb69009ae222eb2db457d070374f9500ad5cc472
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2019-12-06 11:02:24 +0100

    added set variable function

--- src/wc-body.js
+++ src/wc-body.js
@@ -30,12 +30,20 @@ export class WcBody extends LitElement {
         this.facets = [];
     }
 
+    // set the property
+    setPpText(ppText) {
+        // set the property
+        this.ppText = ppText;
+        // initialise the baseterm and facets suggestions
+        this.baseterm = this.facets = [];
+    }
+
     render() {
         return html`
             ${style}
             <div id="body">
                 <!-- component for analysing the food description inserted -->
-                <wc-body-analyser @analysed="${(e) => (this.ppText = e.detail.ppText)}"></wc-body-analyser>
+                <wc-body-analyser @analysed="${(e) => (this.setPpText(e.detail.ppText))}"></wc-body-analyser>
                 <!-- component for tagging the words for baseterm -->
                 <wc-body-classifier .ppText="${this.ppText}" @classified="${(e) => (this.baseterm = e.detail.baseterm, this.facets = e.detail.facets)}"></wc-body-classifier>
                 <!-- component for getting the foodex2 code -->
