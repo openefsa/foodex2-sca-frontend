eb683d3508dfb4ce3f3e482e02ebaebd9b32c4de
SHAHAJ Alban <Alban.SHAHAJ@ext.efsa.europa.eu>
2019-12-17 15:27:47 +0100

    removed usless properties and other optimisations

--- src/wc-body.js
+++ src/wc-body.js
@@ -11,13 +11,7 @@ export class WcBody extends LitElement {
 
     static get properties() {
         return {
-            fe2_codes: {
-                type: Array
-            },
-            baseterm: {
-                type: Array
-            },
-            facets: {
+            codes: {
                 type: Array
             }
         }
@@ -25,35 +19,29 @@ export class WcBody extends LitElement {
 
     constructor() {
         super()
-        this.fe2_codes = [];
-        this.baseterm = [];
-        this.facets = [];
+        this.codes = [];
     }
 
-    // set the property
-    setFE2Codes(fe2_codes) {
-        // set the property
-        this.fe2_codes = fe2_codes;
-        console.log(this.fe2_codes);
-        // initialise the baseterm and facets suggestions
-        this.baseterm = this.facets = [];
+    // updated the returned results
+    updateCode(codes) {
+        this.codes = codes;
     }
 
     render() {
-        return html`
+        return html `
             ${style}
             <div id="body">
                 <!-- component for analysing the food description inserted -->
-                <wc-body-analyser @analysed="${(e) => (this.setFE2Codes(e.detail.fe2_codes))}"></wc-body-analyser>
+                <wc-body-analyser @analysed="${(e) => (this.updateCode(e.detail.codes))}"></wc-body-analyser>
                 <!-- component for getting the foodex2 code -->
-                <wc-body-showcode .codes="${this.fe2_codes}"></wc-body-showcode>
+                <wc-body-showcode .codes="${this.codes}" .text="${this.text}"></wc-body-showcode>
             </div>
         `
     }
 
     /*
     <!-- component for tagging the words for baseterm -->
-    <wc-body-classifier .ppText="${this.ppText}" @classified="${(e) => (this.baseterm = e.detail.baseterm, this.facets = e.detail.facets)}"></wc-body-classifier>
+    <wc-body-classifier-deprecated .ppText="${this.ppText}" @classified="${(e) => (this.baseterm = e.detail.baseterm, this.facets = e.detail.facets)}"></wc-body-classifier-deprecated>
     */
 
 }
