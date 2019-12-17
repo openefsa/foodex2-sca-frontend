032ade4bd6809f8980587cf07d72b501aec7c8c7
SHAHAJ Alban <Alban.SHAHAJ@ext.efsa.europa.eu>
2019-12-17 14:12:17 +0100

    new component used for showing the returned foodex2 code and its english interpretation

---
+++ src/wc-body-showcode.js
@@ -0,0 +1,190 @@
+import {
+    LitElement,
+    html
+} from 'lit-element';
+import {
+    style
+} from './main-styles.js';
+
+export class WcBodyShowCode extends LitElement {
+
+    static get properties() {
+        return {
+            codes: {
+                type: Array
+            },
+            selCode: {
+                type: String
+            },
+            selName: {
+                type: String
+            },
+            index: {
+                type: Number
+            },
+            accuracy: {
+                type: Number
+            }
+        }
+    }
+
+    constructor() {
+        super();
+        this.codes = [];
+        this.selCode = "";
+        this.selName = "";
+        this.accuracy = -1;
+        this.index = -1;
+    }
+
+    render() {
+        return html `
+            ${style}
+            <main>
+                <div>
+                    <label>Description</label>
+                    <div id="tags"></div>
+                </div>
+                <div>
+                    <label>Code</label>
+                    <div class="grid-container">
+                        <div>
+                            <button class="submit-style" @click="${this.prev}"><</button>
+                        </div>
+                        <div class="textarea">
+                            <div>${this.selCode}</div>
+                        </div>
+                        <div>
+                            <button class="submit-style" @click="${this.next}">></button>
+                        </div>
+                    </div>
+                </div>
+                <div class="grid-container">
+                    <p>${this.index+1}/10</p>
+                    <p>Accuracy: ${this.accuracy=Math.round(this.accuracy*100000)}%</p>
+                    <p class="tooltip">
+                        <img src="src/icons/help.png"></img>
+                        <span class="tooltiptext">For additional information check the following <a href="https://www.efsa.europa.eu/en/data/data-standardisation" target="_blank">link</a>.\nInstead, if the returned results are not correct click <a href="#" @click="${this.updateInfo}">here</a>.</span>
+                    </p>
+                </div>
+                <!-- The Modal dialog -->
+                <div id="dialog" class="modal">
+                    <!-- Modal content -->
+                    <div class="modal-content">
+                        <p>Select the baseterm</p>
+                        <select>
+                            <option>A</option>
+                            <option>B</option>
+                        </select>
+                        <button @click="${this.save}">Ok</button>
+                    </div>
+                </div>
+            </main>
+        `
+    }
+
+    // update div ui in specific properties change
+    shouldUpdate(changedProperties) {
+
+        var changed = changedProperties.has('codes');
+        var ui_interaction = changedProperties.has('selCode');
+
+        // if codes has been updated
+        if (changed) {
+            if (this.codes[0]) {
+                this.index = 0;
+                var item = this.codes[this.index];
+                this.selCode = item.code;
+                this.selName = item.name;
+                this.accuracy = item.affinity;
+            }
+        }
+
+        // update tags area
+        this.populateTags();
+
+        return changed || ui_interaction;
+    }
+
+    // select the previous
+    prev() {
+        if (this.index > 0) {
+            this.index -= 1;
+            var item = this.codes[this.index];
+            this.selCode = item.code;
+            this.selName = item.name;
+            this.accuracy = item.affinity;
+        }
+    }
+
+    // select the next
+    next() {
+        if (this.index < 9 && this.index>-1) {
+            this.index += 1;
+            var item = this.codes[this.index];
+            this.selCode = item.code;
+            this.selName = item.name;
+            this.accuracy = item.affinity;
+        }
+    }
+
+    // method used for populating the tags area
+    populateTags() {
+
+        var tagInput = this.shadowRoot.getElementById('tags');
+
+        // if div undefined
+        if (!tagInput)
+            return;
+
+        // clean the content of the element and baseterm
+        tagInput.innerHTML = "";
+
+        var words = this.selName.split(';');
+
+        var type = "bt";
+
+        words.forEach(function (word, i) {
+
+            console.log("iter");
+            if (i > 0) {
+                type = "fc";
+            }
+
+            var tag = document.createElement('TAG');
+            tag.setAttribute('class', 'selected-' + type);
+            tag.innerHTML = word;
+            // append the new inner tag
+            var innerTag = document.createElement('inner-' + type);
+            innerTag.innerHTML = type;
+            tag.appendChild(innerTag);
+
+            tagInput.appendChild(tag);
+        });
+    }
+
+    // updated the information area
+    updateInfo() {
+
+        if(!this.selName || !this.selCode){
+            alert("Codify a term before!");
+            return;
+        }
+
+        // Get the modal
+        var modal = this.shadowRoot.getElementById("dialog");
+        // change modal style in order to show it
+        modal.style.display = "block";
+
+    }
+
+    save(){
+        // Get the modal
+        var modal = this.shadowRoot.getElementById("dialog");
+        // hide modal if ok pressed
+        modal.style.display = "none";
+    }
+
+}
+
+customElements.define("wc-body-showcode", WcBodyShowCode)
