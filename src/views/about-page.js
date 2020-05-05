e80d7d3dff85a2107ce22bd36d4c5f9888a8f1cc
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2020-05-05 16:04:16 +0200

    added header

--- src/views/about-page.js
+++ src/views/about-page.js
@@ -1,3 +1,22 @@
+/*
+ * *********************************************************************
+ * |                                                                    
+ * | File: \src\views\about-page.js
+ * | Project: foodex2-smart-coding-app-frontend
+ * | Created Date: Thursday, April 2nd 2020, 4:42:05 pm
+ * | Author: Alban Shahaj (shahaal)
+ * | Email: data.collection@efsa.europa.eu
+ * | -----------------------------------------------------------------  
+ * | Last Modified: 2nd April 2020
+ * | Modified By: Alban Shahaj (shahaal)
+ * | -----------------------------------------------------------------  
+ * | Copyright (c) 2020 European Food Safety Authority (EFSA)
+ * |                                                                    
+ * *********************************************************************
+ */
+
+
+
 import {
     LitElement,
     html,
