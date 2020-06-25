65e846f82ddf00000b102ff88aefeab853e36717
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2020-06-25 16:38:02 +0200

    added possibility to remember user logged in

--- src/views/login-page.js
+++ src/views/login-page.js
@@ -3,11 +3,11 @@
  * |                                                                    
  * | File: \src\views\login-page.js
  * | Project: foodex2-smart-coding-app-frontend
- * | Created Date: Thursday, April 2nd 2020, 4:42:38 pm
+ * | Created Date: 2nd April 2020
  * | Author: Alban Shahaj (shahaal)
  * | Email: data.collection@efsa.europa.eu
  * | -----------------------------------------------------------------  
- * | Last Modified: 2nd April 2020
+ * | Last Modified: Thursday, 24th June 2020
  * | Modified By: Alban Shahaj (shahaal)
  * | -----------------------------------------------------------------  
  * | Copyright (c) 2020 European Food Safety Authority (EFSA)
@@ -28,16 +28,17 @@ import '@polymer/iron-form/iron-form.js';
 
 
 class User {
-    constructor(user) {
-        this.usr = user;
+    constructor(email, token) {
+        this.email = email;
+        this.token = token;
     }
 
-    getUsername() {
-        return this.usr;
+    getEmail() {
+        return this.email;
     }
 
     isLoggedIn() {
-        return (this.usr != null);
+        return (this.email != null && this.token != null);
     }
 }
 
@@ -52,6 +53,9 @@ export class LoginPage extends LitElement {
                 type: User
             },
             activatePb: {
+                type: Boolean
+            },
+            remember: {
                 type: Boolean
             }
         }
@@ -92,8 +96,10 @@ export class LoginPage extends LitElement {
         super();
         this.user = new User();
         this.activatePb = false;
+        this.remember = JSON.parse(localStorage.getItem('remember'));
         // url to which post user info
-        this.url = new URL('http://51.124.148.195:5000/login');
+        //this.url = new URL('http://51.124.148.195:5000/login');
+        this.url = new URL('http://127.0.0.1:5000/getToken');
     }
 
     render() {
@@ -101,16 +107,19 @@ export class LoginPage extends LitElement {
         <div class="flex-container">
             ${this.user.isLoggedIn()
                 ? html`
-                    <label>Username: ${this.user.getUsername()}</label>
+                    <label>Email: ${this.user.getEmail()}</label>
                     <paper-button raised @click="${this.logout}">Logout</paper-button>
                 `
                 : html`
                     <iron-form id="iron-form">
                         <form>
-                            <paper-input type="text" name="usrname" label="Username" required auto-validate error-message="Username missing"></paper-input>
+                            <paper-input type="email" name="email" label="Email" required auto-validate error-message="Username missing"></paper-input>
                             <paper-input type="password" name="pswd" label="Password" required auto-validate error-message="Please provide a password"></paper-input>
                         </form>
                     </iron-form>
+                    <div>
+                        <input type="checkbox" value="lsRememberMe" .checked="${this.remember}" @click="${this.toggleCheck}"> <label for="rememberMe">Remember me</label>
+                    </div>
                     <paper-button raised @click="${this.login}">Login</paper-button>
                 `
             }
@@ -121,12 +130,36 @@ export class LoginPage extends LitElement {
         `
     }
 
+    /* Called after the element’s DOM has been updated the first time, immediately before updated is called. */
+    firstUpdated() {
+        // get the item from default object
+        let u = JSON.parse(localStorage.getItem('user'));
+        // if a custom value exsists update default one
+        if (u != null) {
+            // update user
+            this.user = new User(u.email, u.token);
+            // fire event to parent
+            this.updateUser();
+        }
+    }
+
+    /* toggle the remember me flag */
+    toggleCheck() {
+        this.remember = !this.remember;
+        localStorage.setItem('remember', this.remember);
+        
+    }
+
     /* save new status on local storage */
     updateUser() {
         // hide progress bar dialog
         this.activatePb = false;
         // save new user on local storage
-        localStorage.setItem("user", JSON.stringify(this.user));
+        if (this.remember) {
+            localStorage.setItem('user', JSON.stringify(this.user));
+        } else {
+            localStorage.removeItem('user');
+        }
         // fire event to parent
         let event = new CustomEvent('userStatus', {
             detail: {
@@ -157,21 +190,21 @@ export class LoginPage extends LitElement {
         // post data
         fetch(this.url, {
             method: 'POST', // or 'PUT'
-            mode: 'no-cors',
             headers: {
+                'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Authorization': 'Basic ' + btoa(data.usrname + ":" + data.pswd)
+                'Authorization': 'Basic ' + btoa(data.email + ':' + data.pswd)
             }
         }).then(res => res.json()
         ).then(res => {
             // allert user
             alert('Login successful.');
             // update user
-            this.user = new User(res.usrname);
+            this.user = new User(data.email, res.token);
             // fire event to parent
             this.updateUser();
         }).catch((err) => {
-            alert('Error or no user found.', err);
+            alert('Error during login. Please contact administartor.', err);
             this.logout();
         });
 
