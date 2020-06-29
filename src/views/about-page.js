ba671589289e21c790f5bc270d01c6fb945ffd48
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2020-06-29 16:51:13 +0200

    update links to repository

--- src/views/about-page.js
+++ src/views/about-page.js
@@ -51,8 +51,8 @@ class AboutPage extends LitElement {
         return html `
         <div class="content">
             <h2>FoodEx2 Smart Coding App v.1.1<h2>
-            <h4>Github frontend: <a href="https://github.com/openefsa/foodex-web-component-frontend" target="_blank">link</a>.</h4>
-            <h4>Github backend: <a href="https://github.com/openefsa/foodex-web-component-backend" target="_blank">link</a>.</h4>
+            <h4>Github frontend: <a href="https://github.com/openefsa/foodex2-sca-frontend" target="_blank">link</a>.</h4>
+            <h4>Github backend: <a href="https://github.com/openefsa/foodex2-sca-backend" target="_blank">link</a>.</h4>
             <h4>Frequently asked questions: <a href="url" target="_blank">link</a>.</h4>
             <h4>EFSA Data Standardisation: <a href="http://www.efsa.europa.eu/en/data/data-standardisation" target="_blank">link</a>.</h4>
             <h4>EFSA web site: <a href="http://www.efsa.europa.eu/en/" target="_blank">link</a>.</h4>
