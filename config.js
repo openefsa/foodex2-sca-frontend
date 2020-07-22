b5c952318d8ff4e5edba4d895d4a713cf10f30d8
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2020-07-22 14:23:56 +0200

    added localhost

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export default {
-    "BASE_URL" : "http://openefsafoodexwebcomponentbackend:5000/"
+    "BASE_URL" : "http://openefsafoodexwebcomponentbackend:5000/" // "http://127.0.0.1:5000/" 
 }
