c7756d2252632de28c121f7690f76567294f6a0f
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2020-11-10 15:53:30 +0100

    new term information panel

---
+++ src/asset/picklist/termDetailLevel.js
@@ -0,0 +1,12 @@
+import {
+    html
+} from 'lit-element';
+
+export default {
+    "H" : html`Hierarchy term <img src="https://raw.githubusercontent.com/openefsa/catalogue-browser/master/src/main/resources/icons/Pyramid.ico"/>`,
+    "M" : "Generic term ⚪",
+    "P" : "Aspecific term 🟡",
+    "C" : "Core term 🔴",
+    "E" : "Extended term 🟢",
+    "F" : html`Facet descriptor <img src="https://raw.githubusercontent.com/openefsa/catalogue-browser/master/src/main/resources/icons/Facet.ico"/>`
+}
--- src/views/home-page.js
+++ src/views/home-page.js
@@ -23,6 +23,29 @@ import {
   css
 } from 'lit-element';
 
+import termType from "../asset/picklist/termType.js";
+import termDetailLevel from "../asset/picklist/termDetailLevel.js";
+
+import '@polymer/paper-icon-button/paper-icon-button.js';
+
+class Term {
+  /**
+   * Constructor of term class to show in term information panel
+   * 
+   * @param {*} obj 
+   */
+  constructor(obj) {
+    this.name = obj.name;
+    this.code = obj.code;
+    this.termType = obj.termType;
+    this.detailLevel = obj.detailLevel;
+    var sc = obj.scopeNote.split("£");
+    this.scopeNote = sc[0];
+    this.links = sc.slice(1);
+  }
+
+}
+
 export class HomePage extends LitElement {
 
   static get properties() {
@@ -39,7 +62,16 @@ export class HomePage extends LitElement {
       facetData: {
         type: Object
       },
+      termToShow: {
+        type: Object
+      },
       text: {
+        type: String
+      },
+      secondPanel: {
+        type: String
+      },
+      iconName: {
         type: String
       },
       loggedIn: {
@@ -50,26 +82,99 @@ export class HomePage extends LitElement {
 
   static get styles() {
     return css`
-    .flex-container {
+    .flexbox {
       display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: stretch;
-      justify-content: stretch; 
+      flex-direction: row;
       height: 100%;
     }
 
-    .flex-container > * {
-      margin: 5px;
+    .main-panel {
+      display: flex;
+      flex-direction: column;
+      flex: 2;
+      width: 200px;
       padding: 5px;
     }
-    
-    .flex-container > .flex-item {
+
+    .info-panel {
+      display: flex;
+      flex-direction: column;
       flex: 1;
+      border-left: 3px solid lightgray;
+      background-color: #f2f2f2;
+      padding: 5px;
     }
-    
-    .flex-container > .raw-item {
-      height: auto;
+
+    .component {
+      margin: 2px;
+    }
+
+    #outer {
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      width: 100%;
+    }
+
+    .fill {
+      flex-grow: 1;
+      max-height: 100vh;
+    }
+
+    .btn {
+      float: right;
+    }
+
+    paper-icon-button {
+      padding: 1px;
+      width: 25px;
+      height: 25px;
+      color: gray;
+    }
+
+    fieldset {
+      border: 1px solid lightgray;
+      border-radius: 4px;
+      max-height: 30px;
+      height: 35px;
+    }
+
+    legend {
+      font-size: 13px;
+    }
+
+    label {
+      white-space: nowrap;
+      font-weight: bold;
+      font-size: 14px;
+    }
+
+    textarea {
+      box-sizing: border-box; /* fit parent width */
+      height: 100%;
+      width: 100%;
+      resize: none;
+    }
+
+    /* On screens that are 600px wide or less, make the columns stack on top of each other instead of next to each other */
+    @media screen and (max-width: 600px) {
+      .flexbox {
+        flex-direction: column;
+      }
+
+      .main-panel {
+        flex-direction: column;
+        min-height: 60%;
+        width: 100%;
+      }
+
+      .info-panel {
+        border-top: 3px solid lightgray;
+        border-left: none;
+        flex-direction: column;
+        min-height: 60%;
+        margin-bottom: 30px;
+      }
     }
 
     `;
@@ -79,37 +184,78 @@ export class HomePage extends LitElement {
     super();
     this.baseterms = null;
     this.facetData = null;
+    this.termToShow = new Object();
     this.selectedBt = null;
     this.selectedFcs = new Array();
     this.text = "";
+    this.secondPanel = "secondPanel";
+    this.iconName = "chevron-left";
   }
 
   render() {
     return html`
-      <div class="flex-container">
-        
-        <!-- component for inserting food description and fetching server -->
-        <input-component class="raw-item" @data="${(e) => (this.updateData(e))}"></input-component>
-  
-        <!-- component for showing baseterm -->
-        <baseterms-component class="raw-item" .baseterms="${this.baseterms}" @bt="${(e) => (this.updateBaseterm(e))}"></baseterms-component>
-      
-        <!-- component for showing facets -->
-        <facets-component class="raw-item" .data="${this.facetData}" @fcs="${(e) => (this.updateFacets(e))}"></facets-component>
-      
-        <!-- component for showing overview of user interaction -->
-        <overview-component class="flex-item" .bt="${this.selectedBt}" .fcs="${this.selectedFcs}"></overview-component>
-      
+      <div class="flexbox">
+        <div class="main-panel">
+          
+          <!-- div with button which shows term information panel -->
+          <div>
+            <paper-icon-button class="btn" icon="${this.iconName}" @click="${this.toggleTermInfoPanel}"></paper-icon-button>
+          </div>
 
-        <!-- component for showing the foodex2 code -->
-        <code-component class="raw-item" .bt="${this.selectedBt}" .fcs="${this.selectedFcs}"></code-component>
-      
-        ${this.loggedIn
-        ? html`
-            <!-- component for activating feedback section -->
-            <feedback-component class="raw-item" .dftDesc="${this.text}"></feedback-component>
-          `
-        : ``}
+          <!-- component for inserting food description and fetching server -->
+          <input-component class="component" @data="${(e) => (this.updateData(e))}"></input-component>
+
+          <!-- component for showing baseterm -->
+          <baseterms-component class="component" .baseterms="${this.baseterms}" @bt="${(e) => (this.updateBaseterm(e))}"></baseterms-component>
+        
+          <!-- component for showing facets -->
+          <facets-component class="component" .data="${this.facetData}" @fcs="${(e) => (this.updateFacets(e))}"></facets-component>
+        
+          <!-- component for showing overview of user interaction -->
+          <overview-component class="component fill" .bt="${this.selectedBt}" .fcs="${this.selectedFcs}" @showInfo="${(e) => (this.showInfo(e))}"></overview-component>
+
+          <!-- component for showing the foodex2 code -->
+          <code-component class="component" .bt="${this.selectedBt}" .fcs="${this.selectedFcs}"></code-component>
+        
+          ${this.loggedIn
+          ? html`
+              <!-- component for activating feedback section -->
+              <feedback-component class="component" .dftDesc="${this.text}"></feedback-component>
+            `
+          : ``}
+        </div>
+        <div id="${this.secondPanel}" class="info-panel" style="display: none;">
+          <div id="outer">
+            <h3>Term information</h3>
+            <fieldset>
+              <legend>Name</legend>
+              <label>${this.termToShow.name}</label>
+            </fieldset>
+            <fieldset>
+              <legend>Code</legend>
+              <label>${this.termToShow.code}</label>
+            </fieldset>
+            <fieldset>
+              <legend>Type</legend>
+              <label>${termType[this.termToShow.termType]}</label>
+            </fieldset>
+            <fieldset>
+              <legend>Level of detail</legend>
+              <label>${termDetailLevel[this.termToShow.detailLevel]}</label>
+            </fieldset>
+            <fieldset class="fill">
+              <legend>Scope notes</legend>
+              <textarea readonly>${this.termToShow.scopeNote}</textarea>
+            </fieldset>
+            <fieldset>
+              <legend>Links</legend>
+              ${(this.termToShow.links && this.termToShow.links.length>0)
+                ? Object.values(this.termToShow.links).map(e => html`<label><a href="${e}" target="_blank">${(new URL(e)).hostname}</a></label>  `)
+                : html`<label>None</label>`
+              }
+            </fieldset>
+          </div>
+        </div>
       </div>
       `
   }
@@ -125,10 +271,11 @@ export class HomePage extends LitElement {
 
     // if new data update categories and selected facets
     if (newSelBt || newSelFcs) {
-      const index = this.selectedFcs.findIndex(f => f.code === this.selectedBt.code);
+      const index = (this.selectedBt)?this.selectedFcs.findIndex(f => f.code === this.selectedBt.code):-1;
       if (index > -1) {
         alert("It is not possible to have the selected baseterm as facet! Please unselect the facet or change baseterm.")
         this.selectedFcs.splice(index, 1);
+        this.selectedBt = null;
       }
     }
 
@@ -141,11 +288,12 @@ export class HomePage extends LitElement {
    */
   updateData(event) {
     var res = event.detail.res;
-    this.baseterms = res[0].baseterm;
-    this.facetData = res[1].facets;
+    this.baseterms = res.bt;
+    this.facetData = res.cat;
     this.text = event.detail.text;
     this.selectedBt = null;
     this.selectedFcs = [];
+    this.termToShow = new Object();
   }
 
   /**
@@ -153,7 +301,7 @@ export class HomePage extends LitElement {
    * @param {*} event 
    */
   updateBaseterm(event) {
-    this.selectedBt = event.detail.selectedBt;
+    this.selectedBt = event.detail;
   }
 
   /**
@@ -161,7 +309,35 @@ export class HomePage extends LitElement {
    * @param {*} event 
    */
   updateFacets(event) {
-    this.selectedFcs = event.detail.selectedFcs.slice(0);
+    this.selectedFcs = event.detail.slice(0);
+  }
+
+  /**
+   * show hide the term informational panel
+   * 
+   */
+  toggleTermInfoPanel () {
+    var x = this.shadowRoot.getElementById(this.secondPanel);
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        this.iconName = "chevron-right";
+    } else {
+        x.style.display = "none";
+        this.iconName = "chevron-left";
+    }
+  }
+
+  /**
+   * show info related to the selected term
+   * @param {*} term 
+   */
+  showInfo(t) {
+    var x = this.shadowRoot.getElementById(this.secondPanel);
+    // show the term information panel if hidden
+    if (x.style.display === "none") {
+      this.toggleTermInfoPanel();
+    }
+    this.termToShow = new Term(t.detail);
   }
 }
 
