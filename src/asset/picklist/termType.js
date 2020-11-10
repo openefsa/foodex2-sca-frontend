b13cde23da2818aa8f430a63e23ffba5f2315d6d
Alban Shahaj <Alban.Shahaj@ad.simbologica1.it>
2020-11-10 15:53:41 +0100

    new term information panel

---
+++ src/asset/picklist/termType.js
@@ -0,0 +1,9 @@
+export default {
+    "n" : "Natural sources",
+    "r" : "Raw primary commodities",
+    "d" : "Raw primary commodities derivatives/ingredients",
+    "s" : "Composite food simple",
+    "c" : "Composite food aggregated",
+    "g" : "Broad or mixed",
+    "f" : "Facet" 
+}
