eac743ce4e010ca2a5601883d9f64a82bbf03276
AlbSha <alban.shahaj@ext.efsa.europa.eu>
2021-08-13 11:14:46 +0200

    updated version

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-    "VERSION" : "1.6",
+    "VERSION" : "1.6.0",
     "BASE_URL" : "https://efsa-ai-prototypes.westeurope.cloudapp.azure.com/foodexwebcomponentbackend/"
     //"BASE_URL" : "http://127.0.0.1:5000/" // only debug
 }
